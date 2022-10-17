6a13cc7b9081e8ff6a127422754d75945c5a3862
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-10-18 06:06:53 +0900

    feat: add testing upload file and updateCards is called

--- components/src/components/Form/Form.test.tsx
+++ components/src/components/Form/Form.test.tsx
@@ -4,8 +4,10 @@ import '@testing-library/react/dont-cleanup-after-each';
 import { BrowserRouter } from 'react-router-dom';
 import Form from './Form';
 import FormPage from 'Components/pages/FormPage/FormPage';
+import userEvent from '@testing-library/user-event';
 
 const updateCards = jest.fn(FormPage.prototype.updateCards);
+global.URL.createObjectURL = jest.fn();
 
 describe('Form', () => {
   beforeEach(() => {
@@ -21,10 +23,10 @@ describe('Form', () => {
     expect(screen.getByRole('form')).toHaveTextContent('Create personal card');
     expect(screen.getByPlaceholderText('Your name...')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Your surname...')).toBeInTheDocument();
+    expect(screen.getByRole('submit-button')).toBeInTheDocument();
   });
 
   test('disabled submit button', () => {
-    expect(screen.getByRole('submit-button')).toBeInTheDocument();
     expect(screen.getByRole('submit-button')).toHaveAttribute('disabled');
   });
 
@@ -36,6 +38,13 @@ describe('Form', () => {
   test('input to have value', () => {
     fireEvent.input(screen.getByPlaceholderText('Your name...'), { target: { value: 'a' } });
     expect(screen.getByPlaceholderText('Your name...')).toHaveValue('a');
+  });
+
+  test('upload file', () => {
+    const fileInput = screen.getByRole('file');
+    const file = new File(['q'], 'q.jpg', { type: 'image/jpeg' });
+    userEvent.upload(fileInput, file);
+    expect((fileInput as HTMLInputElement).files![0]).toBe(file);
   });
 
   test('disabled submit button after click if changes is not at all inputs', () => {
@@ -61,16 +70,29 @@ describe('Form', () => {
     fireEvent.change(screen.getByRole('country'), { target: { value: 'France' } });
     fireEvent.click(screen.getByRole('gender'));
     fireEvent.click(screen.getByRole('checkbox'));
+    const fileInput = screen.getByRole('file');
+    const file = new File(['q'], 'q.jpg', { type: 'image/jpeg' });
+    userEvent.upload(fileInput, file);
     fireEvent.click(screen.getByRole('submit-button'));
-    [
-      'Incorrect name',
-      'Incorrect surname',
-      'Please enter date',
-      'Please enter country',
-      'The gender field is required',
-      'This checkbox is required',
-    ].forEach((message) => {
-      expect(screen.queryByText(message)).not.toBeInTheDocument();
+    const errors = screen.queryAllByRole('error-message');
+
+    errors.forEach((message) => {
+      expect(message).not.toBeInTheDocument();
     });
+  });
+
+  test('updateCards function is called', () => {
+    fireEvent.input(screen.getByPlaceholderText('Your name...'), { target: { value: 'a' } });
+    fireEvent.input(screen.getByPlaceholderText('Your surname...'), { target: { value: 'a' } });
+    fireEvent.change(screen.getByRole('date'), { target: { value: '2022-01-01' } });
+    fireEvent.change(screen.getByRole('country'), { target: { value: 'France' } });
+    fireEvent.click(screen.getByRole('gender'));
+    fireEvent.click(screen.getByRole('checkbox'));
+    const fileInput = screen.getByRole('file');
+    const file = new File(['q'], 'q.jpg', { type: 'image/jpeg' });
+    userEvent.upload(fileInput, file);
+    fireEvent.click(screen.getByRole('submit-button'));
+
+    expect(updateCards).toBeCalledTimes(1);
   });
 });
